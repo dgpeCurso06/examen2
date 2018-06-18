2e3fb0d151544d6ce1003836adef1d98312d8b8d
dgpe curso 06 <dgpe.curso.06@gmail.com>
2018-06-18 18:26:04 -0500

    comento anulados

--- vertx-sample/src/test/java/mx/unam/dgpe/sample/controller/TestMyController.java
+++ vertx-sample/src/test/java/mx/unam/dgpe/sample/controller/TestMyController.java
@@ -11,10 +11,10 @@ public class TestMyController extends AbstractVerticle {
     private static final Logger logger = Logger.getLogger(TestMyController.class);
     //Aqui los test
     //@Test
-    public void ok() throws Exception {
+   /* public void ok() throws Exception {
         String result = sendGet("http://localhost:6060/api/calcula?var1=5");
         logger.info(result);
         assertTrue(false);
-    }
+    }*/
 
 }
